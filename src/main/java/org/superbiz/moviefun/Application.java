a0a818ab0639a7e3809deae42f90e7e6d491d63d
Hou In Choi <hichoi17@outlook.com>
2018-03-01 11:00:12 -0700

    logging finish

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -19,6 +19,11 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Value("${s3.endpointUrl}") String s3EndpointUrl;
+    @Value("${s3.accessKey}") String s3AccessKey;
+    @Value("${s3.secretKey}") String s3SecretKey;
+    @Value("${s3.bucketName}") String s3BucketName;
+
     @Bean
     public ServletRegistrationBean actionServletRegistration(ActionServlet actionServlet) {
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
@@ -34,9 +39,9 @@ public class Application {
         ServiceCredentials serviceCredentials,
         @Value("${s3.endpointUrl:#{null}}") String s3EndpointUrl
     ) {
-        String s3AccessKey = serviceCredentials.getCredential("moviefun-s3", "aws-s3", "access_key_id");
-        String s3SecretKey = serviceCredentials.getCredential("moviefun-s3", "aws-s3", "secret_access_key");
-        String s3BucketName = serviceCredentials.getCredential("moviefun-s3", "aws-s3", "bucket");
+//        String s3AccessKey = serviceCredentials.getCredential("moviefun-s3", "aws-s3", "access_key_id");
+//        String s3SecretKey = serviceCredentials.getCredential("moviefun-s3", "aws-s3", "secret_access_key");
+//        String s3BucketName = serviceCredentials.getCredential("moviefun-s3", "aws-s3", "bucket");
 
         AWSCredentials credentials = new BasicAWSCredentials(s3AccessKey, s3SecretKey);
         AmazonS3Client s3Client = new AmazonS3Client(credentials);
